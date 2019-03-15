82a1927a77b6c8e81575cd7c0b289a66cd8d4971
EamonnMR <eamonn.m.r@gmail.com>
2019-03-15 18:04:14 -0400

    Process the incoming data a bit

--- reddit.py
+++ reddit.py
@@ -6,7 +6,12 @@ def stories_for_sub(subreddit, stories=10):
     headers = {
             "User-Agent": "unix:cursed-internet:v{} (by /u/EamonnMR)".format(VERSION)
     }
-    return requests.get(
+    response = requests.get(
             "http://reddit.com/r/{}/top/.json?count={}".format(subreddit, stories),
             headers=headers)
+
+    response.raise_for_status()
+
+
+    return [x['data']['title'] for x in response.json()['data']['children']]
 
