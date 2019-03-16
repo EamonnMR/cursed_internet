00eff74da6da24625667dba8d15d51151b690e37
EamonnMR <eamonn.m.r@gmail.com>
2019-03-16 16:40:31 -0400

    Method for displaying retreved stories
    Also adds some logging to diagnose the many ugly
    crashes.

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import curses
+from contextlib import contextmanager
+import traceback
+
+
+from reddit import stories_for_sub
 
 SPLASH = '''
    _____                        _
@@ -15,20 +20,50 @@ SPLASH = '''
 
 '''
 
+@contextmanager
+def logger(logfile="log.txt"):
+    output = open(logfile, 'w')
+    try:
+        yield output
+    except Exception as e:
+        output.write("Exception encountered: {}".format(type(e).__name__))
+        output.write(traceback.format_exc())
+    finally:
+        output.flush()
+
+
 class UserInterface:
     def __init__(self):
         self.screen = curses.initscr()
         curses.noecho()
         curses.cbreak()
         self.screen.keypad(True)
+        self.height, self.width = self.screen.getmaxyx()
     def __del__(self):
         curses.nocbreak()
         self.screen.keypad(False)
         curses.echo()
         curses.endwin()
 
-if __name__ == "__main__":
-    ui = UserInterface()
-    ui.screen.addstr(SPLASH)
-    ui.screen.getch()
+    def display_list(self, lines):
+        for line in lines:
+            self.screen.addstr(line)
+            y, x = self.screen.getyx()
+            x = 0
+            y = y + 1
+            if y >= self.height:
+                break
+            self.screen.move(y, x)
+        self.screen.refresh()
 
+if __name__ == "__main__":
+    with logger() as logfile:
+        ui = UserInterface()
+        ui.screen.addstr(SPLASH)
+        ui.screen.refresh()
+        posts = stories_for_sub('linux') 
+        ui.screen.getch()
+        ui.screen.erase()
+        ui.screen.move(0,0)
+        ui.display_list(posts)
+        ui.screen.getch()
