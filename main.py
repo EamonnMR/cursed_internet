dcd68199de53e89d4405fc0f979e6bf925b804df
EamonnMR <eamonn.m.r@gmail.com>
2019-03-25 22:28:51 -0400

    Second level of menus
    Adds an RC file which lists subreddits to offer, adds a second level of menu-
    the first lists subreddits, the second lists stories in that sub.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import curses
 from contextlib import contextmanager
 import traceback
+import json
 
 
 from reddit import stories_for_sub
@@ -49,9 +50,10 @@ class VerticalMenu:
                 self.sel_up()
             if input in (ord('q'), curses.KEY_EXIT):
                 break
-            if input == curses.KEY_ENTER:
-                self.log.write("selection picked: {}\n".format(self.options[self.selection]))
-                break 
+            if input in (ord('\n'), ord('\r'), curses.KEY_ENTER):
+                self.log.write("selection picked: {}\n".format(self.options[self.selection][0]))
+                self.log.flush()
+                self.options[self.selection][1]()
                 # TODO: hang data off to determine what to do here
 
     def sel_up(self):
@@ -61,7 +63,8 @@ class VerticalMenu:
         self.selection = (self.selection - 1) % len(self.options)
         self.update_sel()
     def update_sel(self):
-        self.log.write("Selected: {}\n".format(self.options[self.selection]))
+        self.log.write("Selected: {}\n".format(self.options[self.selection][0]))
+        self.log.flush()
 
 
 class UserInterface:
@@ -78,7 +81,7 @@ class UserInterface:
         curses.endwin()
 
     def display_list(self, lines):
-        for line in lines:
+        for line, _ in lines:
             self.screen.addstr(line)
             y, x = self.screen.getyx()
             x = 0
@@ -88,15 +91,42 @@ class UserInterface:
             self.screen.move(y, x)
         self.screen.refresh()
 
+def load_config():
+    config = json.loads(open("cursedrc.json").read())
+    return config["subreddits"]
+
+def menu_for_story(ui, story, logfile):
+    logfile.write("TODO: Implement showing story comments")
+
+def menu_for_sub(ui, sub, logfile):
+    ui.screen.erase()
+    ui.screen.move(0,0)
+
+    menu = VerticalMenu(
+        ui,
+        [
+            (story, lambda: menu_for_story(ui, story, logfile))
+            for story in 
+            stories_for_sub(sub)
+        ],
+        logfile
+    )
+
+    menu.main()
+
 if __name__ == "__main__":
     with logger() as logfile:
+        subreddits = load_config()
+
         ui = UserInterface()
         ui.screen.addstr(SPLASH)
         ui.screen.refresh()
         ui.screen.getch()
         ui.screen.erase()
         ui.screen.move(0,0)
-        posts = stories_for_sub('linux') 
-        menu = VerticalMenu(ui, posts, logfile)
+        menu = VerticalMenu(ui, [
+            (sub, lambda: menu_for_sub(ui, sub, logfile).main())
+            for sub in subreddits
+        ], logfile)
         menu.main()
         ui.screen.getch()
